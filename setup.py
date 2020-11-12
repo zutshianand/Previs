c3414d71e8a0c5ad592bcb35ef3a673af35346a2
zutshianand <123anandji@gmail.com>
2020-11-12 12:43:46 +0530

    Added setup files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='previs',
+    packages=['previs'],
+    version='1.0',
+    description='PREVIS stands for PREprocessing and VIsualisation. '
+                'This package consists of essential code snippets required for preprocessing and '
+                'visualisation of data science tasks.',
+    author='Anand Zutshi (zutshianand)',
+    author_email='123anandji@gmail.com',
+    url='https://github.com/zutshianand/Previs.git',
+    download_url='https://github.com/zutshianand/Previs/archive/1.0.tar.gz',
+    keywords=['dataloaders', 'preprocessors', 'featuregeneration', 'convenience', 'visualisation', 'pytorch', 'models',
+              'deep-learning', 'dataset', 'snippets'],
+    classifiers=[],
+)
